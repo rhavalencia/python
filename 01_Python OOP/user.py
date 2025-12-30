9d575fd939c0ad6de1a7c32a57be8b607df8750b
rhavalencia <70908464+rhavalencia@users.noreply.github.com>
2025-12-30 03:01:28 +0000

    PMS Project Init

--- 01_Python OOP/user.py
+++ 01_Python OOP/user.py
@@ -1,7 +1,8 @@
 class User:
     def __init__(self, username, email, password):
         self.username = username
-        self.__email = email
+        # self.__email = email
+        self._email = email
         self.password = password
 
     def say_hi_to_user(self, user):
@@ -12,19 +13,26 @@ class User:
     # Accessing and Modifying Data
     # 1. The traditional way: make data private and use getters and setters:
     # prefix data field (email) with '__' name mangling
-    def get_email(self):
-        return self.__email
+    #def get_email(self):
+    #    return self.__email
 
-    def set_email(self, new_email):
-        self.__email = new_email
+    #def set_email(self, new_email):
+    #    self.__email = new_email
 
 
+    # 2. Use of Properties
+    @property
+    def email(self):
+        print("Email accessed")
+        return self._email
 
 user1 = User("Batman", "batman@gotham.com", "imB@tman")
 user2 = User("Robin", "robin@gotham.com", "r0bin")
 
 # user1.say_hi_to_user(user2)
 
-print(user1.get_email())
-user2.set_email("sample@email.com")
-print(user2.get_email())
+#print(user1.get_email())
+#user2.set_email("sample@email.com")
+#print(user2.get_email())
+
+print(user1.email)
